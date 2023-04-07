e13997fe106dd28f34b179cf77c99c5e883cd068
Evan Jackson <evanjack@iu.edu>
2023-04-07 15:29:28 -0400

    Initial commit

---
+++ postingservice/src/main/java/edu/iu/c322/finalproject/postingservice/PostingserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.finalproject.postingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostingserviceApplication.class, args);
+	}
+
+}
---
+++ postingservice/src/test/java/edu/iu/c322/finalproject/postingservice/PostingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.finalproject.postingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
